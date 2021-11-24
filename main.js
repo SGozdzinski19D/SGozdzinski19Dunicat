4d5723e1b1015bfdd33629dd9a5d6f7633165660
SGozdzinski19D <90689572+SGozdzinski19D@users.noreply.github.com>
2021-11-24 21:33:07 +0100

    Dokończenie strony albumu i dodanie strony utworu

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const niedzialajacy = document.querySelectorAll(".niedziala")
+const niedzialajacyalbum = document.querySelectorAll(".niedzialaalbum")
+const niedzialajacanuta = document.querySelectorAll(".niedzialanuta")
 
 function albumniedziala()
 {
-  alert ("Zawartość tego albumu nie została jeszcze wprowadzona na stronę. Spróbuj sprawdzić najnowszy album wybranego przez ciebie artysty, on na pewno działa!");
+  alert ("Zawartość tego albumu nie została jeszcze wprowadzona na stronę. Spróbuj sprawdzić najnowszy album wybranego przez Ciebie artysty, on na pewno działa!");
 }
 
-niedzialajacy.forEach(item => item.addEventListener("click", albumniedziala))
+function nutaniedziala()
+{
+  alert ("Teskt tego utworu nie został jeszcze wprowadzony na stronę. Spróbuj sprawdzić pierwszy utwór z wybranego przez Ciebie albumu, on na pewno działa!");
+}
 
-// for(let i = 0; i < niedzialajacy.length; i++)
-// {
-//     niedzialajacy[i].addEventListener("click", albumniedziala)
-// }
+niedzialajacyalbum.forEach(item => item.addEventListener("click", albumniedziala))
+niedzialajacanuta.forEach(item => item.addEventListener("click", nutaniedziala))
